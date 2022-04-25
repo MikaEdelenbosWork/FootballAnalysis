2a261a7b6c09646ccc0207e2f9464f073344a383
Mika <mikaedelenboswork@gmail.com>
2022-04-25 15:47:48 +0200

    Added a Player vs League Average Radar Rewrote certain data manipulations to make it easier to work on later i.e 39/40. Made a get league average function

--- Projects/DataAnalyzer.py
+++ Projects/DataAnalyzer.py
@@ -1,10 +1,17 @@
-import csv
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 import pandas as pd
-from adjustText import adjust_text
+import plotly.graph_objects as go
+
+
+def get_league_average(dataframe, columnname, decimals):
+    # Get the league average and round to specific decimals
+    temp_total = dataframe[columnname].sum()
+    temp_participants = dataframe[columnname].count()
+    temp_average = temp_total / temp_participants
+    temp_average = temp_average.round(decimals)
+    return temp_average
+
 
 sns.set(style='darkgrid', context='notebook', palette='pastel', font_scale=0.8)
 teamShootingData = []
@@ -12,30 +19,25 @@ teamPassingData = []
 playerShootingData = []
 playerPassingData = []
 
-
-def open_data(data_title):
-    data = []
-    with open(f'../Data/{data_title}', 'r', encoding="utf-8") as csvfile:
-        file_reader = csv.reader(csvfile, delimiter=',')
-        for row in file_reader:
-            data.append(row)
-    data = np.array(data)
-    return data
-
-
 teamShootingData = pd.read_csv('../Data/MLS_Team_Shooting_Data.csv')
 teamPassingData = pd.read_csv('../Data/MLS_Team_Passing_Data.csv')
 playerShootingData = pd.read_csv('../Data/MLS_Player_Shooting_Data.csv')
 playerPassingData = pd.read_csv('../Data/MLS_Player_Passing_Data.csv')
 
 # Filter playing time for at least 90 minutes
-playerShootingData = playerShootingData[playerShootingData.iloc[:, 4] >= 1]
-playerPassingData = playerPassingData[playerPassingData.iloc[:, 4] >= 1]
+playerShootingData = playerShootingData[playerShootingData['90s'] >= 1]
+playerPassingData = playerPassingData[playerPassingData['90s'] >= 1]
 # Filter Position
-playerShootingData = playerShootingData[playerShootingData.iloc[:, 2].str.contains('FW')]
-playerPassingData = playerPassingData[playerPassingData.iloc[:, 2].str.contains('MF')]
+playerShootingData = playerShootingData[playerShootingData['Pos'].str.contains('FW')]
+playerPassingData = playerPassingData[playerPassingData['Pos'].str.contains('MF')]
 playerShootingData.reset_index(inplace=True)
 playerPassingData.reset_index(inplace=True)
+print(playerShootingData)
+print(playerPassingData)
+
+# Filter Player Data
+playerShootingData['Player'] = playerShootingData['Player'].astype('string')
+specificPlayerData = playerShootingData.loc[playerShootingData['Player'] == 'Leonardo Campana']
 
 # ---Team Data---
 tnpxG = teamShootingData['npxG']
@@ -53,8 +55,26 @@ pFWSquad = playerShootingData['Squad']
 pMFSquad = playerPassingData['Squad']
 players = playerShootingData['Player']
 
+# Get League Average
+leagueAverageGoals = get_league_average(playerShootingData, 'Gls', 2)
+leagueAverageNpxG = get_league_average(playerShootingData, 'npxG', 2)
+leagueAverageShots = get_league_average(playerShootingData, 'Sh', 2)
+leagueAverageSoT = get_league_average(playerShootingData, 'SoT', 2)
+leagueAverageGoalsSoT = get_league_average(playerShootingData, 'G/SoT', 2)
+
+# ---Player Specific Data---
+psGoals = specificPlayerData['Gls'].item()
+psNpxG = specificPlayerData['npxG'].item()
+psShots = specificPlayerData['Sh'].item()
+psSoT = specificPlayerData['SoT'].item()
+psGoalsSoT = specificPlayerData['G/SoT'].item()
+
 # ---Plotting the Data---
 fig1, axes = plt.subplots(2, 2)
+fig2 = go.Figure()
+r1 = [psGoals, psNpxG, psShots, psSoT, psGoalsSoT]
+r2 = [leagueAverageGoals, leagueAverageNpxG, leagueAverageShots, leagueAverageSoT, leagueAverageGoalsSoT]
+theta = ['Goals', 'npxG', 'Shots', 'SoT', 'G/SoT']
 fig1.set_figheight(15)
 fig1.set_figwidth(15)
 
@@ -89,4 +109,18 @@ for i, label in enumerate(players):
     if pPasses[i] >= 60 or pProgPasses[i] >= 8:
         axes[1, 1].annotate(label, (pPasses[i], pProgPasses[i]), textcoords="offset points", xytext=(0, 10), ha='center'
                             , arrowprops=dict(arrowstyle="->", connectionstyle="arc3", color='black'))
+
+fig2.add_trace(go.Scatterpolar(r=r1, theta=theta, name='Leonardo Campana', fill='toself', hovertemplate='%{theta}: %{r}'))
+fig2.add_trace(go.Scatterpolar(r=r2, theta=theta, name='League Average', fill='toself', hovertemplate='%{theta}: %{r}'))
+fig2.update_layout(
+    polar=dict(
+        radialaxis=dict(
+            visible=True
+        )),
+    showlegend=True,
+    title='Leonardo Campana Evaluation',
+    hovermode='closest'
+)
+
 plt.show()
+fig2.show()
